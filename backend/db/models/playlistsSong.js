ab1846f1e5442c27cef986bc1faa0e48109b9852
Matthew Hutter <matthewjhutter@gmail.com>
2022-08-01 12:59:09 -0700

    Added Albums, Comments, Playlists, PlaylistsSongs, and Songs. Deleted old readme files

---
+++ backend/db/models/playlistsSong.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class PlaylistsSong extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  PlaylistsSong.init({
+    playlistId: DataTypes.INTEGER,
+    songId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'PlaylistsSong',
+  });
+  return PlaylistsSong;
+};
