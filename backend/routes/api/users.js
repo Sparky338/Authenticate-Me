3a851151d577516b5c8c7165756cbf02c03edd21
Matthew Hutter <matthewjhutter@gmail.com>
2022-08-04 09:05:36 -0700

    Artist error handler working, compiling details of artist by id

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Song } = require('../../db/models');
+const { User, Song, Album,  } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
@@ -41,5 +41,30 @@ router.post(
     });
   }
 );
+
+// Get details of artist (user) from id
+router.get('/:userId', async (req, res) => {
+  const artistId = req.params.userId;
+
+  const artist = await User.findByPk(artistId);
+
+  if (!artist) {
+    return res.json({
+        message: "Artist couldn't be found",
+        statusCode: 404
+    })
+}
+
+  //lazy load songs & albums, sum them. prievew image for each song
+  const totalSongs = await Song.count({
+    where: {userId: artistId}
+  })
+
+  const totalAlbums = await Album.count({
+    where: {userId: artistId}
+  })
+
+  res.json(totalAlbums)
+})
 
 module.exports = router;
