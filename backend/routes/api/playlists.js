4ed967352f4e7215d7a4de960652feb5cf6bef8a
Matthew Hutter <matthewjhutter@gmail.com>
2022-08-04 17:20:03 -0700

    add song to playlist finished, needs bugfix (adding PlaylistId: null and SongId:null)

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Song, Album, Playlist } = require('../../db/models');
+const { User, Song, Album, Playlist, PlaylistsSong } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+//Create a Playlist
 router.post('/', requireAuth, async (req, res) => {
     const user = req.user.id;
     const { name, imageUrl } = req.body;
@@ -29,7 +30,48 @@ router.post('/', requireAuth, async (req, res) => {
         imageUrl
     })
 
+    res.status(201)
     return res.json(newPlaylist)
+})
+
+// Add a Song to a Playlist based on the Playlists's id
+router.post('/:playlistId/songs', requireAuth, async (req, res) => {
+    const user = req.user.id;
+    const playlistId = req.params.playlistId;
+    const { songId } = req.body;
+    const playlist = await Playlist.findByPk(playlistId);
+    const song = await Song.findByPk(songId);
+
+    if (!playlist) {
+        res.status(404)
+        return res.json({
+            message: "Playlist couldn't be found",
+            statusCode: 404
+        })
+    }
+
+    if (user !== playlist.userId) {
+        res.status(401)
+        return res.json({
+            message: "User must be the Playlist's owner",
+            statusCode: 401
+        })
+    }
+
+    if (!song) {
+        res.status(404)
+        return res.json({
+            message: "Song couldn't be found",
+            statusCode: 404
+        })
+    }
+
+    const addSong = await PlaylistsSong.create({
+        playlistId,
+        songId
+    })
+
+    return res.json(addSong)
 })
 
 module.exports = router;
