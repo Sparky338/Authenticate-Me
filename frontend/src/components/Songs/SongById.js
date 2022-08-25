ceeb2edd8a0fbb8fb5af488471278478fc73003d
Matthew Hutter <matthewjhutter@gmail.com>
2022-08-25 14:26:22 -0700

    edit and delete buttons appear correctly on single song when the current user and artist are the same. Help from Rawaha to show that I needed my artistId variable after the rerender of the page for state to be correct

--- frontend/src/components/Songs/SongById.js
+++ frontend/src/components/Songs/SongById.js
@@ -3,12 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { getSongById } from "../../store/songs";
 import DeleteSong from "./DeleteSong";
-import SongsCurrentUser from "./SongsCurrentUser";
 
 const SongById = () => {
     const dispatch = useDispatch();
     const {songId} = useParams();
     const songById = [useSelector(state => state.songs[songId])];
+    const currentUserId = useSelector(state => state.session.user.id)
 
     useEffect(() => {
         dispatch(getSongById(songId));
@@ -18,20 +18,38 @@ const SongById = () => {
     if (!songById[0]) {
         return null
     }
+    const artistId = songById[0].userId
+    console.log('artistId', artistId)
 
-    return (
-        <div>
-            {songById.map((song) => {
-                return (
-                    <li key={song.id}>
-                        Artist Id:{song.userId}, Song Title:{song.title}, Album Id: {song.albumId}
-                        {/* <Link to={`/songs/${song.id}/edit`}>Edit</Link> */}
-                        <DeleteSong />
-                    </li>
-                )
-            })}
-        </div>
-    );
+    if (currentUserId === artistId){
+        return (
+            <div>
+                {songById.map((song) => {
+                    return (
+                        <li key={song.id}>
+                            Artist Id:{song.userId}, Song Title:{song.title}, Album Id: {song.albumId}
+                            <Link to={`/songs/${song.id}/edit`}>Edit</Link>
+                            <DeleteSong />
+                        </li>
+                    )
+                })}
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                {songById.map((song) => {
+                    return (
+                        <li key={song.id}>
+                            Artist Id:{song.userId}, Song Title:{song.title}, Album Id: {song.albumId}
+                            {/* <Link to={`/songs/${song.id}/edit`}>Edit</Link> */}
+                            {/* <DeleteSong /> */}
+                        </li>
+                    )
+                })}
+            </div>
+        );
+    }
 
 }
 
