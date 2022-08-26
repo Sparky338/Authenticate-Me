9142088009687e95142e7b1657493c0b90bb909f
Matthew Hutter <matthewjhutter@gmail.com>
2022-08-26 13:36:49 -0700

    SongById works

--- frontend/src/components/Songs/GetSongs/SongById.js
+++ frontend/src/components/Songs/GetSongs/SongById.js
@@ -5,38 +5,39 @@ import DeleteSong from "../DeleteSongs/DeleteSong";
 
 const SongById = () => {
     const {songId} = useParams();
-    const songObj = useSelector(state => state.songs);
-    // const songs = Object.values(songObj)
-    const songByIdObj = songObj[songId].id //maybe songs[songId]
-    const songById = Object.values(songByIdObj);
     const session = useSelector(state => state.session);
+    const songObj = useSelector(state => state.songs);
+    const songs = Object.values(songObj)
 
     const currentUserId = session.user.id;
     const artistId = songObj[songId].userId;
-    console.log('song by Id', songById);
+    const filteredSong = songs.filter(song => song.id === +songId);
+
+    // const songByIdObj = songObj[songId].id //maybe songs[songId]
+    // const songById = Object.values(songByIdObj);
 
     // if (!songById[0]) {
     //     return null
     // }
 
-    // if (currentUserId === artistId){
-    //     return (
-    //         <div>
-    //             {songById.map((song) => {
-    //                 return (
-    //                     <li key={song.id}>
-    //                         Artist Id:{song.userId}, Song Title:{song.title}, Album Id: {song.albumId}
-    //                         <Link to={`/songs/${song.id}/edit`}>Edit</Link>
-    //                         <DeleteSong />
-    //                     </li>
-    //                 )
-    //             })}
-    //         </div>
-    //     );
-    // } else {
+    if (currentUserId === artistId){
         return (
             <div>
-                {songById.map((song) => {
+                {filteredSong.map((song) => {
+                    return (
+                        <li key={song.id}>
+                            Artist Id:{song.userId}, Song Title:{song.title}, Album Id: {song.albumId}
+                            <Link to={`/songs/${song.id}/edit`}>Edit</Link>
+                            <DeleteSong />
+                        </li>
+                    )
+                })}
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                {filteredSong.map((song) => {
                     return (
                         <li key={song.id}>
                             Artist Id:{song.userId}, Song Title:{song.title}, Album Id: {song.albumId}
@@ -45,7 +46,7 @@ const SongById = () => {
                 })}
             </div>
         );
-    // }
+    }
 
 }
 
