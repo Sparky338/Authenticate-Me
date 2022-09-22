6cd381c4f7102e56159fd9c5e4e6f417ce3a96b9
Matthew Hutter <matthewjhutter@gmail.com>
2022-09-21 21:16:49 -0700

    button styled, needs some more work

--- frontend/src/components/Songs/SongCard.js
+++ frontend/src/components/Songs/SongCard.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import './SongCard.css';
 
 const SongCard = ({ id, userId, albumId, title, description, url, imageUrl }) => {
+    const playIcon = <i class="fa-solid fa-circle-play fa-3x"></i>
     const handlePlay = () => {
 
     }
@@ -10,7 +11,7 @@ const SongCard = ({ id, userId, albumId, title, description, url, imageUrl }) =>
             <li className="song-card">
                 <div className="card-outer">
                     <img className="image card-image" src={`${imageUrl}`} alt={`Song artwork`} />
-                    <button className="play-button button-overlap" onClick={() => handlePlay}>Play button here</button>
+                    <button className="play-button button-overlap" onClick={() => handlePlay}>{playIcon}</button>
                 </div>
                 <Link to={`/songs/${id}`} className="title link card-title">{title}</Link> {/*font-size 14px, line-height: 1.4, text-decoration: none; font-weight 100,
             font-family:Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;*/}
