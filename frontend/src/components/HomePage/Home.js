65aff3375872b7ab742c37df69f67365d34340df
Matthew Hutter <matthewjhutter@gmail.com>
2022-09-23 16:04:39 -0700

    styled home splash page with max 12 cards

--- frontend/src/components/HomePage/Home.js
+++ frontend/src/components/HomePage/Home.js
@@ -1,20 +1,37 @@
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import SongCard from "../Songs/SongCard";
 import './Home.css'
 
 const Home = () => {
     const history = useHistory();
+    const songsSession = useSelector((state) => (state.songs));
 
     if (useSelector(state => state.session.user)) {
         history.push('/songs')
     }
+    if (!songsSession) return null
+
+    const songs = Object.values(songsSession)
+
 
     return (
-        <div>
+        <div className="home-outer outer-div">
             <div className="home trending-text"> Hear what's trending for free in the SoundCloud community</div>
-
+            <div className="song-card-outer">
+                <ul className="songs-ul">
+                    {songs.slice(0,12).map((song) => {
+                        return (
+                            // <div className="allSongs" key={song.id}>
+                            //     <Link to={`/songs/${song.id}`}>Artist Id:{song.userId}, Song Title:{song.title}</Link>
+                            // </div>
+                            <SongCard className="allSongs" key={song.id} {...song} />
+                        )
+                    })}
+                </ul>
+            </div>
             <div className="button-div">
-            <button className="button free-music" onClick={() => history.push('/songs')}>Explore the entire library {/* 12 cards(double row)*/}</button>
+                <button className="button free-music" onClick={() => history.push('/songs')}>Explore the entire library {/* 12 cards(double row)*/}</button>
             </div>
         </div>
 
