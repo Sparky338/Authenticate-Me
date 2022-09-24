ffe9b296e90b0e1a1c8b34f048e18551b8cce2b9
Matthew Hutter <matthewjhutter@gmail.com>
2022-09-24 11:30:46 -0700

    enlarged width of signup buttons, width and height of signup input fields, made margin similar between fields and buttons of login and signup modals

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -26,56 +26,58 @@ function SignupForm() {
   };
 
   return (
-    <form className="modal signup-modal" onSubmit={handleSubmit}>
-      <ul className='error-ul'>
-        {errors.map((error, idx) => <li className="login-errors" key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        {/* Email: */}
-        <input
-          className='input'
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Your email address"
-          required
-        />
-      </label>
-      <label>
-        {/* Username: */}
-        <input
-          className='input'
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Your username"
-          required
-        />
-      </label>
-      <label>
-        {/* Password: */}
-        <input
-          className='input'
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Your password"
-          required
-        />
-      </label>
-      <label>
-        {/* Confirm Password: */}
-        <input
-          className='input'
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          placeholder="Confirm your password"
-          required
-        />
-      </label>
-      <button className="button submit-button" type="submit">Create account</button>
-    </form>
+    <div className="outer-signup-form">
+      <form className="modal signup-form signup-modal" onSubmit={handleSubmit}>
+        <ul className='error-ul'>
+          {errors.map((error, idx) => <li className="login-errors" key={idx}>{error}</li>)}
+        </ul>
+        <label className="input-label">
+          {/* Email: */}
+          <input
+            className='input signup-input'
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Your email address"
+            required
+          />
+        </label>
+        <label className="input-label">
+          {/* Username: */}
+          <input
+            className='input signup-input'
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Your username"
+            required
+          />
+        </label>
+        <label className="input-label">
+          {/* Password: */}
+          <input
+            className='input signup-input'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Your password"
+            required
+          />
+        </label>
+        <label className="input-label">
+          {/* Confirm Password: */}
+          <input
+            className='input signup-input'
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirm your password"
+            required
+          />
+        </label>
+        <button className="button submit-button" type="submit">Create account</button>
+      </form>
+    </div>
   );
 }
 
