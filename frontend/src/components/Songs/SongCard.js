cc3eab07043af69145795c77830a6dbbed5828a1
Matthew Hutter <matthewjhutter@gmail.com>
2022-09-25 11:51:32 -0700

    styled images in cards to be responsive and fit appropriately

--- frontend/src/components/Songs/SongCard.js
+++ frontend/src/components/Songs/SongCard.js
@@ -9,20 +9,20 @@ const SongCard = ({ id, userId, albumId, title, description, url, imageUrl, User
 
     const handlePlay = (url) => {
         dispatch(loadCurrentSong(url))
-//ternary to either dispatch or pause song. ternary to have a play icon or pause icon.
+        //ternary to either dispatch or pause song. ternary to have a play icon or pause icon.
     }
 
     return (
-        <div className="song-cards">
-            <li className="song-card">
-                <div className="card-outer">
-                    <img className="image card-image" src={`${imageUrl}`} alt={`Song artwork`} />
-                    <button className="play-button button-overlap" onClick={() => handlePlay(url)}>{playIcon}</button>
-                </div>
-                <Link to={`/songs/${id}`} className="title link card-title">{title}</Link>
-                <div className="artist card-artist">{User.username}</div> {/*Can change to Link in future when Artist page is setup*/}
-            </li>
-        </div>
+            <div className="song-cards">
+                <li className="song-card">
+                    <div className="card-image-outer">
+                        <img className="image card-image" src={`${imageUrl}`} alt={`Song artwork`} />
+                        <button className="play-button button-overlap" onClick={() => handlePlay(url)}>{playIcon}</button>
+                    </div>
+                    <Link to={`/songs/${id}`} className="title link card-title">{title}</Link>
+                    <div className="artist card-artist">{User.username}</div> {/*Can change to Link in future when Artist page is setup*/}
+                </li>
+            </div>
     );
 };
 
