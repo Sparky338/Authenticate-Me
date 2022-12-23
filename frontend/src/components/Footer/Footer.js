ffda769d9d1ab126e8619686e6946b43d6a84505
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-23 10:46:28 -0800

    created footer

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,46 @@
+import "./footer.css"
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <div className="footer-container">
+                <div className='dev-footer-container'>
+                    <div className='Footer-content'>
+                        <div className="dev-name-footer">
+                            Matthew Hutter
+                        </div>
+                        <div className="dev-links-footer">
+                            <a className="dev-link-footer"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/Sparky338">
+                                GitHub
+                            </a>
+                            <a className="dev-link-footer"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.linkedin.com/in/matthew-hutter-2a6620173">
+                                LinkedIn
+                            </a>
+                            <a className="dev-link-footer"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://sparky338.github.io/">
+                                Profile
+                            </a>
+                            <a className="dev-link-footer"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/Sparky338/SoundClod">
+                                SoundClod Repo
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer
